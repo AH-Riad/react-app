d72ad2f1b7d5cb825e437bca21171d8e7674d167
Riad <riadk910@gmail.com>
2025-04-17 14:17:03 +0600

    added an array in Listgroup

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
+import ListGroup from "./components/ListGroup";
 function App() {
   return (
-    <>
-      <h1>List</h1>
-      <div className="list-group">
-        <a
-          href="#"
-          className="list-group-item list-group-item-action active"
-          aria-current="true"
-        >
-          The current link item
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          A second link item
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          A third link item
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          A fourth link item
-        </a>
-        <a
-          className="list-group-item list-group-item-action disabled"
-          aria-disabled="true"
-        >
-          A disabled link item
-        </a>
-      </div>
-    </>
+    <div>
+      <ListGroup />
+    </div>
   );
 }
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,4 +1,23 @@
-function ListGroup (){
-    return <h1>List Group</h1>
+function ListGroup() {
+  <h1>List Group</h1>;
+  const items = [
+    "Rangpur",
+    "Dhaka",
+    "Khulna",
+    "Chittagong",
+    "Barishal",
+    "Mymenshing",
+    "Shylet",
+  ];
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 export default ListGroup;
