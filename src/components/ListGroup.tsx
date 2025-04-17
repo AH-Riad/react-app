bee9ae9d8bc69a4f6232d147e6c1aac38311baf3
Riad <riadk910@gmail.com>
2025-04-17 20:58:51 +0600

    added a click event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -9,15 +9,20 @@ function ListGroup() {
     "Mymenshing",
     "Shylet",
   ];
-  items = [];
   return (
     <>
       <h1>List</h1>
       {/* condition rendering */}
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item, index)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
